67b8e64c5c66c2da84bf2b5be44ab5bf126b116a
elfiservice <elfiservice@hotmail.com>
2017-12-31 10:59:14 -0300

    feat: secao15-aula86-rota-cadastrar-impl-erros

--- app/controllers/cadastroCtrl.js
+++ app/controllers/cadastroCtrl.js
@@ -1,3 +1,20 @@
 module.exports.cadastro = (application, req, res) => {
-  res.render('cadastro');
+  res.render('cadastro', {validacao: {}, dadosDoForm: {}});
+};
+
+module.exports.cadastrar = (application, req, res) => {
+  var dadosDoForm = req.body;
+
+  req.assert('nome', 'Nome nao pode ser vazio!').notEmpty();
+  req.assert('usuario', 'Usuario nao pode ser vazio').notEmpty();
+  req.assert('senha', 'Senha nao pode ser vazia').notEmpty();
+  req.assert('casa', 'Deve ser selecionada uma Casa!').notEmpty();
+
+  var erros = req.validationErrors();
+  if(erros) {
+    res.render('cadastro', {validacao: erros, dadosDoForm: dadosDoForm});
+    return;
+  }
+
+  res.send('OK.. Pode cadastrar');
 };
--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -2,4 +2,9 @@ module.exports = function(application){
 	application.get('/cadastro', function(req, res){
 		application.app.controllers.cadastroCtrl.cadastro(application, req, res);
 	});
+
+	//vem da view cadastro.ejs do form metodo post ao enviar um cadatro
+	application.post('/cadastrar', (req, res) => {
+		application.app.controllers.cadastroCtrl.cadastrar(application, req, res);
+	});
 }
