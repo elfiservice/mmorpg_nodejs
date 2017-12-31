3278d42124eea8b91538f896bd32ec74e29998cb
elfiservice <elfiservice@hotmail.com>
2017-12-31 10:26:54 -0300

    chore: secao15-aula85-implemt-routers-controllers

--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -1,5 +1,5 @@
 module.exports = function(application){
 	application.get('/cadastro', function(req, res){
-		res.render('cadastro')
+		application.app.controllers.cadastroCtrl.cadastro(application, req, res);
 	});
 }
