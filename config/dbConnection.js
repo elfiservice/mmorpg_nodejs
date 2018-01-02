b914208cfad1db1d9eabfefbec6722a71a21e577
elfiservice <elfiservice@hotmail.com>
2018-01-02 08:12:41 -0300

    chore: secao15-aula88-criando-conexao-mongodb

---
+++ config/dbConnection.js
@@ -0,0 +1,21 @@
+// Importar o Modulo do MongoDB
+var mongo = require('mongodb');
+
+// cria uma Função-variavel para retornar uma conexao quando necessario na APlicação
+var connection = () => {
+  var db = new mongo.Db(
+    'got', //nome do banco de dados da aplicaçao Game of Thrones
+    new mongo.Server(
+      'localhost', //string com endereço do servidor
+      27017, //porta de conexao padrão
+      {} //objeto vazio, config adicionais
+    ),
+    {} //config adicionais
+  );
+
+  return db;
+}
+
+module.exports = () => {
+  return connection;
+};
