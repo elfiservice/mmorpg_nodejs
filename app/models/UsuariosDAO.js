001462ba21551d996842d7589710f519e355f076
Armando Jr <elfiservice@hotmail.com>
2018-01-03 08:17:45 -0300

    fix: secao15-aula89-corrigido-funcao-inserirCadastro

--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -2,7 +2,7 @@ function UsuariosDAO(connection) {
   this._connection = connection();
 }
 
-UsuariosDAO.prototype.inserirUsuario = (usuario) => {
+UsuariosDAO.prototype.inserirUsuario = function(usuario) {
   //funcai open fornecida pelo objeto mongo.Db
   this._connection.open( (err, mongoClient) => {
     //estabelecida a conexao, agora podemos manipular os docs dentro do bd
