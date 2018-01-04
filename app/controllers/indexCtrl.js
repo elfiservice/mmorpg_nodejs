e02fa1eb890890cc86f5f6dce5ff2b70296a8ebd
Armando Jr <elfiservice@hotmail.com>
2018-01-04 07:38:48 -0300

    feat: secao16-aula93-preparando-rotas

--- app/controllers/indexCtrl.js
+++ app/controllers/indexCtrl.js
@@ -1,3 +1,19 @@
 module.exports.index = (application, req, res) => {
-  res.render('index');
+  res.render('index', {validacao: {}});
+};
+
+module.exports.autenticar = (application, req, res) => {
+  var dados = req.body;
+
+  req.assert("usuario", "Usuario nao deve ser vazio").notEmpty();
+  req.assert("senha", "Senha nao deve ser vazio").notEmpty();
+
+  var erros = req.validationErrors();
+  if(erros) {
+    res.render("index", {validacao: erros});
+    return;
+  }
+
+  res.send("pode cadastrar!")
+
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,4 +2,8 @@ module.exports = function(application){
 	application.get('/', function(req, res){
 		application.app.controllers.indexCtrl.index(application, req, res);
 	});
+
+	application.post('/autenticar', function(req, res){
+		application.app.controllers.indexCtrl.autenticar(application, req, res);
+	});
 }
