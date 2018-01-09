6206b26e30ce3275374e6a0f2aebcd4e6aeb588d
Armando Jr <elfiservice@hotmail.com>
2018-01-09 18:22:09 -0300

    feat: secao16-aula97-destroindo-sessao

--- app/controllers/jogoCtrl.js
+++ app/controllers/jogoCtrl.js
@@ -5,3 +5,9 @@ module.exports.jogo = (application, req ,res) => {
     res.send("Usuario não logado!");
   }
 };
+
+module.exports.sair = (application, req, res) => {
+  req.session.destroy(function(err) {
+    res.render('index', {validacao: {}});
+  });
+};
--- app/routes/jogo.js
+++ app/routes/jogo.js
@@ -2,4 +2,8 @@ module.exports = (application) => {
   application.get('/jogo', (req, res) => {
     application.app.controllers.jogoCtrl.jogo(application, req, res);
   });
+
+  application.get('/sair', (req, res) => {
+    application.app.controllers.jogoCtrl.sair(application, req, res);
+  });
 };
