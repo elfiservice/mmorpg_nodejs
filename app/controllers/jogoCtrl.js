132406e15f6baa5253f36e1290a8ad0216a9eb0f
Armando Jr <elfiservice@hotmail.com>
2018-01-09 18:04:46 -0300

    feat: secao16-aula96-na-pg-jogo-verifica-user-logado

--- app/controllers/jogoCtrl.js
+++ app/controllers/jogoCtrl.js
@@ -1,3 +1,7 @@
 module.exports.jogo = (application, req ,res) => {
-  res.render('jogo');
+  if(req.session.autorizado) {
+    res.render('jogo');
+  } else {
+    res.send("Usuario não logado!");
+  }
 };
